910009ed2f3db99441fb00600f6a4c9637e43d59
Yuri Bychkov <yuribychkov15@gmail.com>
2024-10-21 18:43:41 -0400

    heroku2 fix

--- cs412/settings.py
+++ cs412/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6=mc8(!$62yb%d)j3$@o73=$yqb8o=dhseo0-7aw5e9b-ffdhu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -128,8 +128,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
