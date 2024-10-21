8caa31e93d1fc6fae449075e0e964b12ba04047c
Yuri Bychkov <yuribychkov15@gmail.com>
2024-10-21 19:35:35 -0400

    heroku8 fix

--- cs412/settings.py
+++ cs412/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
