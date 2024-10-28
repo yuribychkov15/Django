806e02b11fc8f67164ad9a001fb8f2b3abeef441
Yuri Bychkov <yuribychkov15@gmail.com>
2024-10-28 11:40:42 -0400

    added friends for profiles

--- mini_fb/admin.py
+++ mini_fb/admin.py
@@ -11,3 +11,4 @@ from .models import *
 admin.site.register(Profile)
 admin.site.register(StatusMessage)
 admin.site.register(Image)
+admin.site.register(Friend)
--- mini_fb/models.py
+++ mini_fb/models.py
@@ -29,6 +29,16 @@ class Profile(models.Model):
         '''Return a URL to display this profile'''
         return reverse('show_profile', kwargs={'pk': self.pk})
     
+    def get_friends(self):
+        # get the friendships
+        friends1 = Friend.objects.filter(profile1=self).values_list('profile2', flat=True)
+        friends2 = Friend.objects.filter(profile2=self).values_list('profile1', flat=True)
+        # combine both
+        friend_ids = set(list(friends1) + list(friends2))
+        # get Profile instance
+        friends = Profile.objects.filter(id__in=friend_ids)
+        return list(friends)
+    
 class StatusMessage(models.Model):
     ''' Encapsulate the idea of one Status Message '''
     message = models.TextField(blank=False)
@@ -52,3 +62,11 @@ class Image(models.Model):
     def __str__(self):
         '''Return a string representation of the object'''
         return f'{self.image_file} {self.timestamp} {self.status_message}'
+
+class Friend(models.Model):
+    profile1 = models.ForeignKey(Profile, related_name='profile1', on_delete=models.CASCADE)
+    profile2 = models.ForeignKey(Profile, related_name='profile2', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f"{self.profile1.first_name} {self.profile1.last_name} & {self.profile2.first_name} {self.profile2.last_name}"
